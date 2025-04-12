f78ad067c9f75b1781fe6c9d29b82334d3102381
noelh03 <houdaelhasni03@gmail.com>
2025-04-12 03:25:22 -0400

    fetch battery percentage

--- projet/app/src/main/java/com/example/polyhike/NavManagerActivity.kt
+++ projet/app/src/main/java/com/example/polyhike/NavManagerActivity.kt
@@ -1,16 +1,24 @@
 package com.example.polyhike
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.polyhike.databinding.ActivityNavManagerBinding
+import com.example.polyhike.ui.home.HomeFragmentCallback
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class NavManagerActivity: AppCompatActivity() {
 
+class NavManagerActivity: AppCompatActivity(), HomeFragmentCallback {
     private lateinit var binding: ActivityNavManagerBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,11 +26,8 @@ class NavManagerActivity: AppCompatActivity() {
 
         binding = ActivityNavManagerBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_container)
-
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_record, R.id.navigation_profile
@@ -30,5 +35,36 @@ class NavManagerActivity: AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onActionTriggered() {
+        loadBatteryInfo()
+    }
+
+    private fun loadBatteryInfo() {
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED)
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED)
+        registerReceiver(batteryInfoReceiver, intentFilter)
+    }
+
+    private val batteryInfoReceiver = object: BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            updateBatteryData(intent)
+        }
+    }
+
+    private fun updateBatteryData(intent: Intent) {
+        var present: Boolean = intent.getBooleanExtra(BatteryManager.EXTRA_PRESENT, false)
+        if (present) {
+            val textView: TextView = findViewById<TextView>(R.id.text_home)
+            val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            if (level != -1 && scale != -1)
+                textView.text = "Battery Pct : ${((level / scale.toFloat()) * 100f).toInt()} %"
+        } else {
+            Toast.makeText(this, "No Battery present", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- projet/app/src/main/java/com/example/polyhike/ui/home/HomeFragment.kt
+++ projet/app/src/main/java/com/example/polyhike/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.polyhike.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,14 +13,15 @@ import com.example.polyhike.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
     private val binding get() = _binding!!
+    private var callback: HomeFragmentCallback? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         val homeViewModel =
             ViewModelProvider(this)[HomeViewModel::class.java]
 
@@ -27,14 +29,34 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+
+        callback?.onActionTriggered()
         return root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is HomeFragmentCallback) {
+            callback = context
+        } else {
+            throw RuntimeException("$context must implement FragmentCallback")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        callback = null
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+}
+
+interface HomeFragmentCallback {
+    fun onActionTriggered()
 }
